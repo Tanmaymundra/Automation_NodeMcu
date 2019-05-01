627deec5bc61b0499de8c9dc33fb184e28883deb
Tanmaymundra <tanmaymundra01@gmail.com>
2019-05-01 19:35:48 +0530

    Update index.h

--- index.h
+++ index.h
@@ -3,7 +3,7 @@ const char page[] PROGMEM = R"=====(
 
 <html>
 <title>	Web Automation </title>
-<!var R1,R2,R3,R4,R5,R6,R7,R8>
+
 <style type="text/css">
 	h1{
 		font-family: courier,courier-new,serif;
